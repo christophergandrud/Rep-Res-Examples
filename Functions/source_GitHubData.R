c2fd6eb25af1c3769080d8520d99ec0950fdee3f
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-01-12 15:51:05 +0900

    request

--- Functions/source_GitHubData.R
+++ Functions/source_GitHubData.R
@@ -1,7 +1,7 @@
 #####################
 # R function for downloading plain-text data from GitHub
 # Christopher Gandrud
-# 7 January 2013
+# 9 January 2013
 #####################
 
 # source_GitHubData is directly based on source_url from the Hadley Wickham's devtools package
@@ -11,7 +11,7 @@ source_GitHubData <-function(url, sep = ",", header = TRUE)
   require(httr)
   request <- GET(url)
   stop_for_status(request)
-  handle <- textConnection(text_content(request))
+  handle <- textConnection(content(request, as = 'text'))
   on.exit(close(handle))
   read.table(handle, sep = sep, header = header)
 }
