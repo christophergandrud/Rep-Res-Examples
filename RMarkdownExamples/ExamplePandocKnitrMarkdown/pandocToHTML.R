25060e7f908ac1ebe53111e0b1b518d8e10018f5
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-01-15 16:49:59 +0900

    end of day examples

--- RMarkdownExamples/ExamplePandocKnitrMarkdown/pandocToHTML.R
+++ RMarkdownExamples/ExamplePandocKnitrMarkdown/pandocToHTML.R
@@ -1,12 +1,25 @@
+############
+# pandocToHTML function for rendering Markdown documents with Pandoc in R
+#  uses bibliographies
+# Christopher Gandrud
+# 15 January 2013
+############
+
 
 #### Function for rendering MD to HTML with Pandoc
-pandocToHTML <-  function(inputFile, outputFile) 
-{      
-  system(paste("pandoc", shQuote(inputFile), "-o", shQuote(outputFile), "--bibliography PandocBib.bib --smart"))
-}
+pandocToHTML <-  
+  function(inputFile, outputFile) 
+  {      
+    system(paste("pandoc", shQuote(inputFile), "-o", shQuote(outputFile), "--bibliography PandocBib.bib --css Markdown.css"))
+  }
 
-setwd("/git_repositories/Rep-Res-Examples/RMarkdownExamples/ExamplePandocKnitrMarkdown/")
 
-knitr::knit("PandocKnitrMarkdown.Rmd", "PandocKnitrMarkdown.md")
+#### Example ####
+# Set working directory to where your .Rmd file is located
+# setwd()
 
-pandocToHTML("PandocKnitrMarkdown.md", "PandocKnitrMarkdown.html")
+# Knit R Markdown file to .md
+# knitr::knit("PandocKnitrMarkdown.Rmd", "PandocKnitrMarkdown.md")
+
+# Render .md in .html with Pandoc
+# pandocToHTML("PandocKnitrMarkdown.md", "PandocKnitrMarkdown.html")
