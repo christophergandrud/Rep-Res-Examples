25631fa1e17f281ae9740f63abcc6a140ee8c99f
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-02-17 17:08:45 +0900

    map update

--- Graphs/GoogleVisMap.R
+++ Graphs/GoogleVisMap.R
@@ -1,16 +1,14 @@
 ##################
 # Create a gvisGeoMap of Global Fertilizer Consumption
 # Christopher Gandrud
-# 7 January 2012
+# 17 February 2013
 ##################
 
 #### Load data used in this document #### 
 # Load packages
 library(devtools)
 library(googleVis)
-
-# Load source_GitHubData function
-source_gist("4466237")
+library(repmis)
 
 # Load data from GitHub (http://bit.ly/V0ldsf)
 ## The data gathering process used to create this data set
@@ -20,6 +18,9 @@ MainData <- source_GitHubData("http://bit.ly/V0ldsf")
 
 # Subset MainData so that it only includes 2003
 SubData <- subset(MainData, year == 2003)
+
+# Remove values of FertilizerConsumption less than 0.1
+SubData <- subset(SubData, FertilizerConsumption > 0.1)
 
 # Find the natural logarithm of FertilizerConsumption.
 ## Round the results to one decimal digit.
