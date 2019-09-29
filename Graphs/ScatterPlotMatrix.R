4525170932a86c33afeb6b45d80eb0f22f7cba78
Christopher Gandrud <christopher.gandrud@gmail.com>
2019-09-29 12:58:58 +0200

    version 3

--- Graphs/ScatterPlotMatrix.R
+++ Graphs/ScatterPlotMatrix.R
@@ -1,27 +1,23 @@
 ##############
 # Create scatterplot matrix from MainData.csv
 # Christopher Gandrud
-# 23 April 2012
+# 29 September 2019
 ##############
 
 #  Load packages
 library(devtools)
-library(repmis)
-
-# Load source_GitHubData
-source_url("http://bit.ly/UOMkpd")
 
 # Download data
-MainData <- source_data("http://bit.ly/V0ldsf")
+main_data <- rio::import("http://bit.ly/V0ldsf",
+                        format = "csv" )
 
-# Subset MainData so that it only includes 2003
-SubData <- subset(MainData, year == 2003)
+# Subset main_data so that it only includes the year 2003
+data_sub <- subset(main_data, year == 2003)
 
 # Remove iso2c, country, year variables
 # Keep reg_4state, disproportionality, FertilizerConsumption
-SubData <- SubData[, c("reg_4state",
-                       "disproportionality",
+data_sub <- data_sub[, c("reg_4state", "disproportionality",
                        "FertilizerConsumption")]
 
-# Create scatterplot matrix
-pairs(x = SubData)
+# Create a scatterplot matrix
+pairs(x = data_sub)
